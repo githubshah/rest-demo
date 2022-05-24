42f8989fdedf53f5818eb1284ca48a3dc2021f10
githubshah <72010111+githubshah@users.noreply.github.com>
2022-05-24 13:40:18 +0530

    donr

--- src/main/java/com/example/restdemo/RestDemoApplication.java
+++ src/main/java/com/example/restdemo/RestDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestDemoApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/restdemo/controller/UserController.java
+++ src/main/java/com/example/restdemo/controller/UserController.java
@@ -1,17 +1,41 @@
 package com.example.restdemo.controller;
 
 
+import org.apache.tomcat.util.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.List;
 
 
 @RestController
 public class UserController {
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/emp")
-    public List<Emp> getAllEmp(){
+    public List<Emp> getAllEmp() {
+
+        abar();
+
+        //logger.info("yeee: " + Emp.loadData());
         return Emp.loadData();
+    }
+
+    private void abar() {
+        try {
+            throw new RuntimeException("hello java");
+        } catch (Exception e) {
+            String s = Arrays.toString(e.getStackTrace());
+            logger.info(">>> "+s);
+
+        }
     }
 }
